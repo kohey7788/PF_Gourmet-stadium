d66543ccd2c66290f458bbf9ec4f22b183839af3
kohey7788 <kohey.chelsea1017@gmail.com>
2021-03-24 14:15:13 +0000

    add public design

--- app/controllers/public/gourmet_comments_controller.rb
+++ app/controllers/public/gourmet_comments_controller.rb
@@ -1,12 +1,11 @@
 class Public::GourmetCommentsController < ApplicationController
   def create
-    byebug
     comment = GourmetComment.new(gourmet_comment_params)
     comment.gourmet_id = params[:gourmet_id]
     comment.visitor_id = current_visitor.id
     comment.save
-    #redirect_back(fallback_location: YOUR_PAGE)
-    redirect_to gourmet_path(params[:gourmet_id])
+    redirect_back(fallback_location: gourmet_path(params[:gourmet_id]))
+    # redirect_to gourmet_path(params[:gourmet_id])
   end
 
   private
