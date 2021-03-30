6b1e00ca2c05703410c56f16b4bb6f864599d01e
kohey7788 <kohey.chelsea1017@gmail.com>
2021-03-30 06:03:10 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    gourmets_path
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+     admin_visitors_path
+    else
+     gourmets_path
+    end
   end
 
   protected
