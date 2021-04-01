783d9e5aa024a3e623e27a79936376b4fc573151
kohey7788 <kohey.chelsea1017@gmail.com>
2021-04-01 09:52:20 +0000

    add image

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-Admin.create!(
-   email: 'test@test.com',
-   password: 'testtest'
-)
-# Stadium.create!(
-#   [
-#     {
-#       name: "国立競技場",
-#       image_id: open("app/asset/images/kokuritu.jpeg"),
-#       address: "東京都新宿区霞ヶ丘町10番1号",
-#     },
-
-#     {
-#       name: "埼玉スタジアム2002",
-#       image_id: open("app/asset/images/Saitama_stadium.png"),
-#       address: " 埼玉県さいたま市緑区美園2丁目1",
-#     }
-#   ]
-
+# Admin.create!(
+#    email: 'test@test.com',
+#    password: 'testtest'
 # )
+
+Stadium.create!(
+   name: "埼玉スタジアム2002",
+   image: open("app/assets/images/stadium.bg.jpeg"),
+   address: "埼玉県さいたま市緑区美園2丁目1"
+)
+
