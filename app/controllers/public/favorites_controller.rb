fe09deeb468ed0f9fd90a7116d02df53391f3c11
kohey7788 <kohey.chelsea1017@gmail.com>
2021-04-08 14:54:30 +0000

    非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,19 +1,19 @@
 class Public::FavoritesController < ApplicationController
   def create
-    gourmet = Gourmet.find(params[:gourmet_id])
-    favorite = current_visitor.favorites.new(gourmet_id: gourmet.id)
+    @gourmet = Gourmet.find(params[:gourmet_id])
+    favorite = current_visitor.favorites.new(gourmet_id: @gourmet.id)
     favorite.save
-    gourmet.favorites_count += 1
-    gourmet.save
-    redirect_to gourmet_path(gourmet)
+    @gourmet.favorites_count += 1
+    @gourmet.save
+    # redirect_to gourmet_path(gourmet)
   end
 
   def destroy
-    gourmet = Gourmet.find(params[:gourmet_id])
-    favorite = current_visitor.favorites.find_by(gourmet_id: gourmet.id)
+    @gourmet = Gourmet.find(params[:gourmet_id])
+    favorite = current_visitor.favorites.find_by(gourmet_id: @gourmet.id)
     favorite.destroy
-    gourmet.favorites_count -= 1
-    gourmet.save
-    redirect_to gourmet_path(gourmet)
+    @gourmet.favorites_count -= 1
+    @gourmet.save
+    # redirect_to gourmet_path(gourmet)
   end
 end
