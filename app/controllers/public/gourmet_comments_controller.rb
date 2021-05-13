d7d0b186f2977497348c5b368bc9cf8c5253b930
kohey7788 <kohey.chelsea1017@gmail.com>
2021-05-13 14:32:12 +0000

    add comment

--- app/controllers/public/gourmet_comments_controller.rb
+++ app/controllers/public/gourmet_comments_controller.rb
@@ -1,10 +1,11 @@
 class Public::GourmetCommentsController < ApplicationController
   def create
-    comment = GourmetComment.new(gourmet_comment_params)
-    comment.gourmet_id = params[:gourmet_id]
-    comment.visitor_id = current_visitor.id
-    comment.save
-    redirect_back(fallback_location: gourmet_path(params[:gourmet_id]))
+    @comment = GourmetComment.new(gourmet_comment_params)
+    @comment.gourmet_id = params[:gourmet_id]
+    @comment.visitor_id = current_visitor.id
+    @comment.save
+    render "index"
+    # redirect_back(fallback_location: gourmet_path(params[:gourmet_id]))
     # redirect_to gourmet_path(params[:gourmet_id])
   end
 
